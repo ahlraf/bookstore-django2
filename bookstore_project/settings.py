49e80a5844f2847d065baa4657820afcb7a2d68d
ahlraf <72507078+ahlraf@users.noreply.github.com>
2021-06-11 04:30:29 +0530

    Adding static assets and third party crispy forms

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'crispy_forms',
+
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
@@ -126,6 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfields')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView #, AboutPageView
+from .views import HomePageView, AboutPageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -40,27 +40,27 @@ class HomepageTests(SimpleTestCase):
 
 
 
-# class AboutPageTests(SimpleTestCase):
+class AboutPageTests(SimpleTestCase):
 
-#     def setUp(self):
-#         url = reverse('about')
-#         self.response = self.client.get(url)
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
 
-#     def test_aboutpage_status_code(self):
-#         self.assertEqual(self.response.status_code, 200)
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
-#     def test_aboutpage_template(self):
-#         self.assertTemplateUsed(self.response, 'about.html')
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
 
-#     def test_aboutpage_contains_correct_html(self):
-#         self.assertContains(self.response, 'About Page')
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
 
-#     def test_aboutpage_does_not_contain_incorrect_html(self):
-#         self.assertNotContains(self.response, 'I should not be here')
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'I should not be here')
 
-#     def test_aboutpage_url_resolves_aboutpageview(self):
-#         view = resolve('/about/')
-#         self.assertEqual(
-#             view.func.__name__,
-#             AboutPageView.as_view().__name__
-#         )
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
